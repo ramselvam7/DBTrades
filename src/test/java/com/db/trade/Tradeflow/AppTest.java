c799d1cc528027b4f3180c2cd90e6b285d7d528e
ramnathselvam <51364228+ramselvam7@users.noreply.github.com>
2021-05-28 19:41:07 +0530

    Update AppTest.java

--- src/test/java/com/db/trade/Tradeflow/AppTest.java
+++ src/test/java/com/db/trade/Tradeflow/AppTest.java
@@ -6,6 +6,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.db.trade.Tradeflow.model.TradeDetails;
 import com.db.trade.Tradeflow.repository.TradeDao;
+import com.db.trade.Tradeflow.service.TradeServiceImpl;
 
 import junit.framework.TestCase;
 import org.junit.Test;
@@ -20,6 +21,9 @@ public class AppTest  extends TestCase
 {
    @Autowired
    private TradeDao tradeDao;   
+   
+   @Autowired
+   private TradeServiceImpl tradeServiceImpl;
 
     @Test
 	public void testSaveRetrieveTrade() throws ParseException
@@ -34,10 +38,58 @@ public class AppTest  extends TestCase
 		tradeDetails.setExpiredInd("N");
 		tradeDao.save(tradeDetails);
 		
-		TradeDetails td = tradeDao.findByTradeID("T1");
+		TradeDetails td = tradeDao.findByTradeID("T1");		
+		
 		assertNotNull(td);
 		assertEquals(tradeDetails.getBookId(), td.getBookId());
-		
-	}   
+	
+	}  
+    
+    @Test
+   	public void testVersion() throws ParseException
+   	{
+       	TradeDetails tradeDetails = new TradeDetails();
+   		tradeDetails.setTradeID("T1");
+   		tradeDetails.setVersion(3);
+   		tradeDetails.setCounterPartyID("C-1");
+   		tradeDetails.setBookId("B1");
+   		tradeDetails.setCreatedDate(new Date());
+   		tradeDetails.setMaturityDate(new SimpleDateFormat("dd/MM/yyyy").parse("01/01/2025"));
+   		tradeDetails.setExpiredInd("N");
+   		tradeDao.save(tradeDetails);
+   		
+	   		
+   		TradeDetails tradeDetailsVersion = new TradeDetails();
+   		tradeDetailsVersion.setTradeID("T1");
+   		tradeDetailsVersion.setVersion(5);
+   		tradeDetailsVersion.setCounterPartyID("C-1");
+   		tradeDetailsVersion.setBookId("B1");
+   		tradeDetailsVersion.setCreatedDate(new Date());
+   		tradeDetailsVersion.setMaturityDate(new SimpleDateFormat("dd/MM/yyyy").parse("01/01/2025"));
+   		tradeDetailsVersion.setExpiredInd("N");
+   		
+   		String result = tradeServiceImpl.persistUpdateTrade(tradeDetails);
+   		
+   		assertEquals(result, "Update");
+
+   	}  
+    
+    @Test
+   	public void testTradeMaturityDate() throws ParseException
+   	{
+       	TradeDetails tradeDetails = new TradeDetails();
+   		tradeDetails.setTradeID("T5");
+   		tradeDetails.setVersion(3);
+   		tradeDetails.setCounterPartyID("C-1");
+   		tradeDetails.setBookId("B1");
+   		tradeDetails.setCreatedDate(new Date());
+   		tradeDetails.setMaturityDate(new SimpleDateFormat("dd/MM/yyyy").parse("01/01/2025"));
+   		tradeDetails.setExpiredInd("N");
+   		
+   		String result = tradeServiceImpl.persistUpdateTrade(tradeDetails);
+   		
+   		assertEquals(result, "Savesuccess");
+	
+   	}  
    
 }
